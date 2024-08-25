26a0e2eacf316c86065e3cf1394ac49d1c5cdc83
gireeswar18 <gireeswarcp18@gmail.com>
2024-08-25 19:11:03 +0530

    25/08/2024

---
+++ AgainTwentyFive.java
@@ -0,0 +1,13 @@
+package codeforces;
+
+import java.util.Scanner;
+
+public class AgainTwentyFive {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String n = sc.next();
+
+        System.out.println(25);
+    }
+}
